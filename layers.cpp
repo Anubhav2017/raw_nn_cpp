b0a759c35ed6cbfef32cebe7f1f6916ad0962a46
Anubhav <agarwalanubhav.2011@gmail.com>
2022-04-17 15:11:43 +0530

    working

--- layers.cpp
+++ layers.cpp
@@ -1,61 +1,252 @@
 #include<vector>
 
-void forward_fcc(vector<float> &x, vector<float,float> &w, vector<float> &y, vector<float> &b, int xdim, int ydim){
+void forward_fcc(vector<float> &x, vector<float> &w, vector<float> &y, vector<float> &b, int xdim, int ydim){
 
-    for(int i=0; i< ydim;i++){
-        y[i]= b[i];
+    float xbuf[xdim];
+    float wbuf[ydim][xdim];
+    float ybuf[ydim];
+    float bbuf[ydim];
 
-        for (int j=0; j<xdim;j++){
-            y[i]+= w[i][j]*x[j];
+    for(int i=0;i<xdim;i++){
+        xbuf[i] = x[i];
+    }
+
+    for(int i=0;i<ydim;i++){
+        for(int j=0;j<xdim;j++){
+            wbuf[i][j] = w[i*xdim+j];
+        }
+    }
+
+    for(int i=0;i<ydim;i++){
+        bbuf[i] = b[i];
+    }
+
+    for(int i=0;i<ydim;i++){
+        ybuf[i] = bbuf[i];
+        for(int j=0;j<xdim;j++){
+            ybuf[i] += xbuf[j]*wbuf[i][j];
+        }
+    }
+
+
+
+    for(int i=0;i<ydim;i++){
+        y[i] = ybuf[i];
+    }
+
+}
+
+void forward_conv(vector<float> &x, vector<float> &w, vector<float> &y, vector<float> &b, int F, int C, int H, int W, int FH, int FW){
+
+    float xbuf[C][H][W];
+    float wbuf[F][C][FH][FW];
+
+    int outH=H-FH+1;
+    int outW=W-FW+1;
+
+    float ybuf[F][outH][outW];
+    float bbuf[F];
+
+    for(int i=0;i<C;i++){
+        for(int j=0;j<H;j++){
+            for(int k=0;k<W;k++){
+                xbuf[i][j][k] = x[i*H*W+j*W+k];
+            }
+        }
+        
+    }
+
+    for(int i=0;i<F;i++){
+        for(int j=0;j<C;j++){
+            for(int k=0;k<FH;k++){
+                for(int l=0;l<FW;l++){
+                    wbuf[i][j][k][l] = w[i*C*FH*FW+j*FH*FW+k*FW+l];
+                }
+            }
+        }
+    }
+
+    for(int i=0;i<ydim;i++){
+        bbuf[i] = b[i];
+    }
+
+    for(int f=0;f<F;f++){
+        for(int c=0;c<C;c++){
+            for(int h=0;h<outH;h++){
+                for(int w=0;w<outW;w++){
+                    ybuf[f][h][w]=bbuf[f];        
+                    for(int fh=0;fh<FH;fh++){
+                        for(int fw=0;fw<FW;fw++){
+                            ybuf[f][h][w] += xbuf[c][h+fh][w+fw]*wbuf[f][c][fh][fw];
+                        }
+                    }
+                }
+            }
         }
     }
 
 }
 
-void backward_fcc(vector<float> &x, vector<float,float> &w, vector<float> &b, vector<float> &dx, vector<float> &dy,int xdim,int ydim,float lr){
+void backward_fcc(vector<float> &x, vector<float> &w, vector<float> &dx, vector<float> &dy,vector<float> &dw, vector<float> &db, int xdim,int ydim){
     //compute gradient of activations
     
-    vector<float> db(ydim);
-    vector<float,float> dw(ydim,xdim);
-    
+    float xbuf[xdim];
+    float wbuf[ydim][xdim];
+    float dxbuf[xdim];
+    float dwbuf[ydim][xdim];
+    float dybuf[ydim];
+    float dbbuf[ydim];
+
     for(int i=0;i<xdim;i++){
-        dx[i]=0;
+        xbuf[i] = x[i];
+    }
+
+
+    for(int i=0;i<ydim;i++){
+        dbbuf[i] = db[i];
+    }
+
+    for(int i=0;i<ydim;i++){
+        for(int j=0;j<xdim;j++){
+            dwbuf[i][j] = dw[i*xdim+j];
+        }
+    }
+
+    for(int i=0;i<ydim;i++){
+        for(int j=0;j<xdim;j++){
+            wbuf[i][j] = w[i*xdim+j];
+        }
+    }
+
+    for(int i=0;i<ydim;i++){
+        dybuf[i] = dy[i];
     }
     
-    for(int j=0;j<xdim;j++){
-        for(int i=0;i<ydim;i++){
-            dx[j] += dy[i] * w[i][j];
+    for(int i=0;i<ydim;i++){
+        for(int j=0;j<xdim;j++){
+            dxbuf[j] = dybuf[i] * wbuf[i][j];
         }
         
     }
     //compute gradient of weights
     for(int i=0;i<ydim;i++){
         for(int j=0;j<xdim;j++){
-            dw[i][j] = dy[i]*x[j];
+            dwbuf[i][j] += dybuf[i]*xbuf[j];
         }
     }
 
     //compute gradient of biases
     for (int i=0;i<ydim;i++){
-        db[i] = dy[i];
+        dbbuf[i] += dybuf[i];
     }
-    
+
+    for(int i=0;i<xdim;i++){
+         dx[i]=dxbuf[i];
+    }
+
+    for(int i=0;i<ydim;i++){
+         db[i]=dbbuf[i];
+    }
+
     for(int i=0;i<ydim;i++){
         for(int j=0;j<xdim;j++){
-            
-            w[i][j]-=lr*dw[i][j];
-            
+            dw[i*xdim+j]=dwbuf[i][j] ;
         }
-        
-        b[i] -= lr*db[i];
     }
+
+    for(int i=0;i<ydim;i++){
+        for(int j=0;j<xdim;j++){
+            w[i*xdim+j] =  wbuf[i][j] ;
+        }
+    }
+    
+    // for(int i=0;i<ydim;i++){
+    //     for(int j=0;j<xdim;j++){
+            
+    //         w[i][j]-=lr*dw[i][j];
+            
+    //     }
+        
+    //     b[i] -= lr*db[i];
+    // }
 }
 
 
-void flatten_forward(vectors)
+void backward_conv(vector<float> &x, vector<float> &w, vector<float> &y, vector<float> &dx,vector<float> &dw,vector<float> &db, vector<float> &dy , int F, int C, int H, int W, int FH, int FW){
 
-void conv_forward(vector<float,float,float> &x, vector<float,float,float,float> &w, vector<float,float,float,float> &y, vector<float> &b, int xdim, int ydim){
+    //populate cache
+    float xbuf[C][H][W];
+    float wbuf[F][C][FH][FW];
+    float bbuf[F];
+
+    for(int i=0;i<C;i++){
+        for(int j=0;j<H;j++){
+            for(int k=0;k<W;k++){
+                xbuf[i][j][k] = x[i*H*W+j*W+k];
+            }
+        }
+        
+    }
+
+
+    for(int i=0;i<F;i++){
+        for(int j=0;j<C;j++){
+            for(int k=0;k<FH;k++){
+                for(int l=0;l<FW;l++){
+                    wbuf[i][j][k][l] = w[i*C*FH*FW+j*FH*FW+k*FW+l];
+                }
+            }
+        }
+    }
+
+    for(int i=0;i<ydim;i++){
+        bbuf[i] = b[i];
+    }
+
+    //dimensions of output
+    int outH=H-FH+1;
+    int outW=W-FW+1;
+
+    //incoming gradient
+    float ybuf[F][outH][outW];
+    float dybuf[F][outH][outW];
+
+    for(int i=0;i<F;i++){
+        for(int j=0;j<outH;j++){
+            for(int k=0;k<outW;k++){
+                dybuf[i][j][k] = dy[i*outH*outW+j*outW+k];
+            }
+        }
+    }
+
+
+    //gradients to be calculated
+
+    float dxbuf[C][H][W];
+    float dwbuf[F][C][FH][FW];
+    float dbbuf[F];
 
     
 
+    
+
+    for(int f=0;f<F;f++){
+        for(int c=0;c<C;c++){
+            for(int h=0;h<outH;h++){
+                for(int w=0;w<outW;w++){
+                    ybuf[f][h][w]=bbuf[f];        
+                    for(int fh=0;fh<FH;fh++){
+                        for(int fw=0;fw<FW;fw++){
+                            ybuf[f][h][w] += xbuf[c][h+fh][w+fw]*wbuf[f][c][fh][fw];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
+// void flatten_forward(vectors)
+    
+
+// }
